13d9dd82d66cc87ad428d11a38a237f58ad20769
sergio-rocha1 <sergio.rocha.borges@gmail.com>
2023-06-03 18:40:51 -0300

    Adicionado a mesma logica de busca da API porém com uma tela utilizando JFrame e exibindo o resultado na própria tela em um campo do tipo JTextArea;

---
+++ src/br/com/consultacep/executavel/PrincipalComTela.java
@@ -0,0 +1,11 @@
+package br.com.consultacep.executavel;
+
+import br.com.consultacep.modelos.TelaFrame;
+
+public class PrincipalComTela {
+
+    public static void main(String[] args) {
+        TelaFrame telaFrame = new TelaFrame();
+        telaFrame.setVisible(true);
+    }
+}
